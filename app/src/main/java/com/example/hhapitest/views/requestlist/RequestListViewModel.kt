ecf9790915f20fefd73c4edf9a54aac4bb66b687
rusrst <69162196+rusrst@users.noreply.github.com>
2022-01-28 20:05:57 +0300

    Update RequestListViewModel.kt

--- app/src/main/java/com/example/hhapitest/views/requestlist/RequestListViewModel.kt
+++ app/src/main/java/com/example/hhapitest/views/requestlist/RequestListViewModel.kt
@@ -3,7 +3,6 @@ package com.example.hhapitest.views.requestlist
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.Transformations
 import com.example.foundation.model.*
 import com.example.foundation.model.tasks.dispatchers.Dispatcher
 import com.example.foundation.model.tasks.factories.TaskFactory
@@ -15,15 +14,12 @@ import com.example.foundation.views.BaseViewModel
 import com.example.foundation.views.LiveResult
 import com.example.foundation.views.MutableLiveResult
 import com.example.hhapitest.model.data.database.RoomRepository
-import com.example.hhapitest.model.data.dataclassesforjson.Area
 import com.example.hhapitest.model.data.dataclassesforjson.ListRequest
 import com.example.hhapitest.model.data.dataclassesforjson.ShortItem
-import com.google.gson.Gson
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 typealias Listener = (String)->Unit
-typealias LiveListShortItem = LiveData<List<ShortItem>?>
 
 class RequestListViewModel(
     screen: RequestList.Screen,
@@ -35,18 +31,6 @@ class RequestListViewModel(
     dispatcher: Dispatcher,
     private val roomRepository: RoomRepository
 ) : BaseViewModel(dispatcher, taskFactory) {
-    //listener is needed to update the value of a variable
-    //we don't subscribe to the original return value in the fragment
-
-    /*private val dataListener: DataListener = {result ->
-        when(result){
-            is ErrorResult -> _data.postValue(result)
-            is PendingResult -> _data.postValue(PendingResult())
-            is SuccessResult -> _data.postValue(result)
-        }
-    }
-
-     */
 
     var urlItem: String? = null
     fun tryAgain(){
@@ -55,20 +39,8 @@ class RequestListViewModel(
         fun getListRequestFromUrl() = load()
 
 
-        private val _data = MutableLiveResult<String>(PendingResult())
-        private var _liveListShortItem: LiveResult<List<ShortItem>?> = Transformations.map(_data) { result ->
-            if (result is SuccessResult){
-                try{
-                val data = Json.decodeFromString<ListRequest>(result.data)
-                return@map SuccessResult(data.items)}
-                catch (e: Exception){
-                    throw e
-                    return@map ErrorResult(e)
-                }
-            }
-            return@map result as Result<List<ShortItem>?>
-        }
-        val liveListShortItem: LiveResult<List<ShortItem>?> = _liveListShortItem
+        private val _data = MutableLiveResult<List<ShortItem>?>(PendingResult())
+        val liveListShortItem: LiveResult<List<ShortItem>?> = _data
         fun launch (screen: BaseScreen, result: Any?){
             navigator.launch(screen, result)
         }
@@ -76,9 +48,10 @@ class RequestListViewModel(
     private fun load(){
         if (urlItem == null) navigator.goBack()
         taskFactory.async {
-            repository.getRequestFromUrl(urlItem ?: "", null)
+            val data= repository.getRequestFromUrl(urlItem ?: "", null)
+            val list = Json.decodeFromString<ListRequest>(data)
+            return@async list.items
         }.into(_data)
-
     }
 }
 
